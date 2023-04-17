77303a4a2b15a24fe688c394ff1d09bfcd3e196a
SamSvetlik <sam.svetlik@gmail.com>
2023-04-16 20:43:23 -0500

    chore: began building json for character data, broke character controller

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,7 +1,13 @@
+const characters = require('../characters.json')
+
 const char = async (req, res) => {
     const input = req.params.character
-    console.log(req.params)
-    res.send(`You requested info on ${input}`)
+    console.log(input)
+    console.log(characters)
+    console.log(characters.input)
+    if (characters.input) {
+        res.send(`You requested info on ${input}`)
+    } else res.send('Character not found')
 }
 
 module.exports = {char}
